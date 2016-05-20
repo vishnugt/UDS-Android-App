f26420cca8e8e742596f589c4649a18a54e7ca4b
vishnu.g <vishnugt@hotmail.com>
2016-05-20 17:30:26 +0530

    second activity created

---
+++ app/src/main/java/in/uds/vishnugt/alpha3/ClientSelection_Activity.java
@@ -0,0 +1,13 @@
+package in.uds.vishnugt.alpha3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+public class ClientSelection_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.client_activity);
+    }
+
+}
--- app/src/main/java/in/uds/vishnugt/alpha3/login_activity.java
+++ app/src/main/java/in/uds/vishnugt/alpha3/login_activity.java
@@ -1,13 +1,31 @@
 package in.uds.vishnugt.alpha3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class login_activity extends AppCompatActivity {
-    //Radheshyam
+    EditText username;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+
+        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
+        {
+            Intent intent = new Intent(this, ClientSelection_Activity.class);
+            this.startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "Credentials wrong >.<", Toast.LENGTH_SHORT).show();
+        }
     }
 }
