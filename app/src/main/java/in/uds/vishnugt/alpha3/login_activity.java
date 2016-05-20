41d62af4a57a727675255087d22c09cfc0043dad
vishnu.g <vishnu.g@uds.local>
2016-05-20 15:29:25 +0530

    Ïnitial Commit

---
+++ app/src/main/java/in/uds/vishnugt/alpha3/login_activity.java
@@ -0,0 +1,13 @@
+package in.uds.vishnugt.alpha3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
