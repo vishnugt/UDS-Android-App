945bbd5fe0970b142de85029b98fd393b27c0a2f
radheshyam.k <radheshyam.k@uds.local>
2016-05-20 15:58:16 +0530

    RV

--- app/src/main/java/in/uds/vishnugt/alpha3/login_activity.java
+++ app/src/main/java/in/uds/vishnugt/alpha3/login_activity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class login_activity extends AppCompatActivity {
-    //Vishnugt
+    //Radheshyam
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
