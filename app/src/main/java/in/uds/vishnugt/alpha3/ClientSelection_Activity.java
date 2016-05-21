757a09d50d3f95a0abc3c15ba6a2f75bdcfe705b
vishnu.g <vishnugt@hotmail.com>
2016-05-21 11:19:59 +0530

    Recycle - Card Partially done

--- app/src/main/java/in/uds/vishnugt/alpha3/ClientSelection_Activity.java
+++ app/src/main/java/in/uds/vishnugt/alpha3/ClientSelection_Activity.java
@@ -1,6 +1,7 @@
 package in.uds.vishnugt.alpha3;
 
 import android.app.ListActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,36 +10,66 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
 
 import java.util.ArrayList;
 
-public class ClientSelection_Activity extends ListActivity {
+public class ClientSelection_Activity extends ActionBarActivity {
 
 
-    ArrayList<String> array= new ArrayList<>();
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            array.add("Apple");
-            array.add("Apple");
-            array.add("Apple");
-            array.add("Apple");
-            array.add("Apple");
-            array.add("Apple");
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "CardViewActivity";
 
-            setListAdapter(new ArrayAdapter<String>(this, R.layout.single_listview_row,array));
-            ListView listView = getListView();
-            listView.setTextFilterEnabled(true);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-                    // When clicked, show a toast with the TextView text
-                    Toast.makeText(getApplicationContext(),
-                            ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-                }
-            });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
 
-        }
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyRecyclerViewAdapter(getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
 
+        // Code to Add an item with default animation
+        //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
+
+        // Code to remove an item with default animation
+        //((MyRecyclerViewAdapter) mAdapter).deleteItem(index);
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(LOG_TAG, " Clicked on Item " + position);
+            }
+        });
+    }
+
+    private ArrayList<DataObject> getDataSet()
+    {
+        ArrayList results = new ArrayList<DataObject>();
+        for(int index=0;index<20;index++)
+        {
+            DataObject obj = new DataObject("Some Primary Text " + index,
+                    "Secondary " + index);
+            results.add(index, obj);
+        }
+        return results;
+    }
+}
