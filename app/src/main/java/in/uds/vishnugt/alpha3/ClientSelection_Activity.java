7fe7cd79aec4cffa1e25b47789912ef7ea7e1ccf
radheshyam.k <krishshyam007@gmail.com>
2016-05-21 12:11:31 +0530

    Dialog box added

--- app/src/main/java/in/uds/vishnugt/alpha3/ClientSelection_Activity.java
+++ app/src/main/java/in/uds/vishnugt/alpha3/ClientSelection_Activity.java
@@ -1,7 +1,10 @@
 package in.uds.vishnugt.alpha3;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,28 +20,28 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
-
 import java.util.ArrayList;
 
-public class ClientSelection_Activity extends ActionBarActivity {
-
-
-
+public class ClientSelection_Activity extends Activity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private static String LOG_TAG = "CardViewActivity";
+    ArrayList results;
+    AlertDialog alertDialog;
+    ArrayList<String> company=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
 
+        results = new ArrayList<DataObject>();
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new MyRecyclerViewAdapter(getDataSet());
+        mAdapter = new MyRecyclerViewAdapter(results);
         Log.e("Error",getDataSet().toString());
         mRecyclerView.setAdapter(mAdapter);
     }
@@ -52,18 +55,37 @@ public class ClientSelection_Activity extends ActionBarActivity {
             @Override
             public void onItemClick(int position, View v) {
                 Log.i(LOG_TAG, " Clicked on Item " + position);
-            }
+                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(v.getContext());
+                alertDialogBuilder.setTitle("Confirm Action");
+                alertDialogBuilder.setMessage(company.get(position));
+
+                alertDialogBuilder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Toast.makeText(ClientSelection_Activity.this,"You clicked yes button",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+                alertDialog = alertDialogBuilder.create();
+                alertDialog.show();            }
         });
     }
 
     private ArrayList<DataObject> getDataSet()
     {
-        ArrayList results = new ArrayList<DataObject>();
         for(int index=0;index<20;index++)
         {
             DataObject obj = new DataObject("Some Primary Text " + index,
                     "Secondary " + index);
             results.add(index, obj);
+            company.add(index,"Some Primary Text "+index);
         }
         return results;
     }
