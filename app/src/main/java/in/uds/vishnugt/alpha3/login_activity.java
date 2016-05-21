643a2b8ab2d4865faf508697dadf48365e373d7a
vishnu.g <vishnugt@hotmail.com>
2016-05-21 18:07:19 +0530

    ÜRL added for reference

--- app/src/main/java/in/uds/vishnugt/alpha3/login_activity.java
+++ app/src/main/java/in/uds/vishnugt/alpha3/login_activity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+//url http://192.168.0.172:8080/flow/rest/login?loggedIn=false&password=%22cat%22&rememberMe=false&requestList=&transactions=&txnlocations=&userId=0&username=%22cats%22
+
+
 public class login_activity extends AppCompatActivity {
     EditText username;
     EditText password;
