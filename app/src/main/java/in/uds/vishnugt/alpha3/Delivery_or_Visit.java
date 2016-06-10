1df7da29bee58020cc29271eb4c4dd6f293b572e
vishnu.g <vishnugt@hotmail.com>
2016-06-10 12:02:05 +0530

    Material Activity Created

--- app/src/main/java/in/uds/vishnugt/alpha3/Delivery_or_Visit.java
+++ app/src/main/java/in/uds/vishnugt/alpha3/Delivery_or_Visit.java
@@ -1,7 +1,9 @@
 package in.uds.vishnugt.alpha3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Delivery_or_Visit extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Delivery_or_Visit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_delivery_supervisor);
+    }
+
+    public void material_delivery_btn_function(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), Material_Activity.class);
+        startActivity(intent);
     }
 }
